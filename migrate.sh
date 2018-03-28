3353e64ce821207a430ba15c0984dda2dc673fe3
mercury <udtrokia@163.com>
2018-03-28 17:38:21 +0800

    graphene bundle update

--- migrate.sh
+++ migrate.sh
@@ -1,14 +1,14 @@
 local=/Users/mercurio/documents/work/stone/
 remote=/Users/mercurio/documents/work/BlockGeek.io/blockgeek/web/
 
+rm -r ./graphene
+gatsby build --prefix-paths
+mv public graphene
+
 git add .
 git commit -m 'graphene bundle update'
 git push lab lazyload
 git push origin lazyload
-
-rm -r ./graphene
-gatsby build --prefix-paths
-mv public graphene
 
 cd ../BlockGeek.io/blockgeek/web/graphene && git pull
 

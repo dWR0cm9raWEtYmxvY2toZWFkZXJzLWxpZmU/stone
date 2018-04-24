86d81f7f39bf16fbe1db5a3eaffd2d17faac5d4c
mercury <udtrokia@163.com>
2018-04-24 12:01:10 +0800

    graphene bundle update

--- src/components/Cp/index.js
+++ src/components/Cp/index.js
@@ -3,11 +3,11 @@ import Grid from "material-ui/Grid";
 import React from "react";
 
 // render list
-var list = ["d45","ac","bbt","bcb","be","bn","br","ce","cs","cu","cw","fm","ge","he","me","te","the","tl","xo"]
+var list = ["bkb","bbt","45q","jscj","ltx","wlcj","bbqkl","BTCmedia","jj","kfztt","kyzg","wl","sycj","lxcj","hqcj","hwbq","jncj","lsw","tnqkl","tljz","31N","lsqkl","wdqkl","hxcj","lj","yfc","hx","sdzcj","tlcj","mrbd","bbd","bqbk","lsj","jdcj","jkb","rolainews","wlw","rgcj","kdbt"]
 var grids = list.map((dir)=>{
 	return (
 		<Grid item>
-			<div className="cctn partner" style={{backgroundImage: "url(" + require(`../../assets/imgs/cpm/${dir}.jpg`) + ")"}} />
+			<div className="cctn partner" style={{backgroundImage: "url(" + require(`../../assets/imgs/cpm/${dir}.png`) + ")"}} />
 		</Grid>
 	)
 })
